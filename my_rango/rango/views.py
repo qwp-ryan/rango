02995c0b539c760352d5238b94af973a81832b0e
qwp-ryan <qwpsinapsdu@aliyun.com>
2017-08-23 22:08:05 +0800

    media files chapter 6

--- my_rango/rango/views.py
+++ my_rango/rango/views.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rango.models import Category
 
 from django.http import HttpResponse
 
 def index(Request):
-    context_dict={'boldmessage':"Crunch, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict={'categories':category_list}
     return render(Request, 'rang/index.html', context = context_dict)
 #    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'> About </a>")
 
