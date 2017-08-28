0c9ba8d9264d40b36e5c7034d67cada99ecc2c73
qwp-ryan <qwpsinapsdu@aliyun.com>
2017-08-28 11:26:16 +0800

    修正错误

--- my_rango/rango/models.py
+++ my_rango/rango/models.py
@@ -2,18 +2,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.template.defaultfilters import slugify
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes= models.IntegerField(default=0)
+    slug = models.SlugField(blank = True, unique = True)
+
+    def save(self, *args, **kwargs):
+        self.slug=slugify(self.name)
+        super(Category,self).save(*args,**kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
         
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
@@ -23,6 +28,6 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
--- my_rango/rango/views.py
+++ my_rango/rango/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from rango.models import Category
+from rango.models import Category, Page
 
 from django.http import HttpResponse
 
@@ -16,3 +16,14 @@ def about(Request):
     context_dict={'newmessage': "about page of Django!"}
     return render(Request, 'rang/about.html', context = context_dict)
     #return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'> Index </a>")
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.object.get(slug = category_name_slug)
+        pages = Page.objects.filter(category = category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request,'rang/category.html',context_dict)
