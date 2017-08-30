6782be5641eb106a5c004b3b34051c622315e6cb
qwp-ryan <qwpsinapsdu@aliyun.com>
2017-08-30 23:38:32 +0800

    foreign time test

--- my_rango/rango/models.py
+++ my_rango/rango/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.template.defaultfilters import slugify
+from datetime import timezone
 # Create your models here.
 
 class Category(models.Model):
@@ -57,12 +58,18 @@ class Project(models.Model):
     time_back = models.DateField(verbose_name='返回中国国境日期', )
     Members = models.ManyToManyField(Personal_information, verbose_name='出访人员')
     Task_detail = models.TextField(verbose_name='任务内容以及意义描述', )
-
+    log = models.TextField(verbose_name = '进度记录',default = self.category)
 #    url = models.URLField()
 #    views = models.IntegerField(default=0)
 #    likes = models.IntegerField(default = 0)
 
-    complete_time
+    def put_log(self,*args, **kwargs):
+        if (self.category != self.log):
+            self.log.append(self.category)
+            self.log.append(timezone.now)
+            super(Project, self).save(*args, **kwargs)
+
+
 
 
     def __str__(self):
