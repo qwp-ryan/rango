265e7e7b8bb894b565b58e6bfefeb80390a46b8f
qwp-ryan <qwpsinapsdu@aliyun.com>
2017-09-18 07:45:49 +0800

    use html template 3

--- my_rango/rango/templatetags/rango_template_tags.py
+++ my_rango/rango/templatetags/rango_template_tags.py
@@ -3,5 +3,6 @@ from rango.models import Category
 
 register = template.Library()
 @register.inclusion_tag('rang/cats.html')
-def get_category_list():
-    return {'cats': Category.objects.all()}
+def get_category_list(cat=None):
+    return {'cats': Category.objects.all(),
+            'act_cat':cat}
