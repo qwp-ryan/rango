57ac69520eb671398bccae16c83d96f64de046b1
qwp-ryan <qwpsinapsdu@aliyun.com>
2017-09-23 22:24:37 +0800

    session

--- my_rango/rango/views.py
+++ my_rango/rango/views.py
@@ -11,15 +11,27 @@ from django.contrib.auth import authenticate, login
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 
 def index(Request):
+#    Request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict={'categories':category_list}
+    page_list=Page.objects.order_by('-views')[:5]
+
+    context_dict={'categories':category_list,'pages':page_list}
+    response=render(Request.'rang/index_1.html',context_dict)
+    visitor_cookie_handler(request,response)
+    reture response
+
     return render(Request, 'rang/index_1.html', context = context_dict)
 #    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'> About </a>")
 
 def about(Request):
+    if Request.session.test_cookie_worked():
+        print ("TEST COOKIE WORKED!")
+        Request.session.delete_test_cookie()
+
     print(Request.method)
     print(Request.user)
     context_dict={'newmessage': "about page of Django!"}
@@ -143,3 +155,20 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def visitor_cookie_handler(request,response):
+    visits=int(request.COOKIES.get('visit','1'))
+
+    last_visit_cookie=request.COOKIES.get('last_visit',str(datetime.now()))
+    last_visit_time=datetime.strptime(last_visit_cookie[:-7],
+                                      '%Y-%m-%d %H:%M:%S')
+    if(datetime.now()-last_visit_time).days>0:
+        visits = visits+1
+        response.set_cookie('last_visit',str(datetime.now()))
+    else:
+        visits = 1
+        response.set_cookie('last_visit',last_visit_cookie)
+
+    response.set_cookie('visits',visits)
+
+
