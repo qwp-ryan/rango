eaaa68713a319af747591e2db62e8d782ed2f6b3
qwp-ryan <qwpsinapsdu@aliyun.com>
2017-10-11 17:00:28 +0800

    buget summary

--- my_rango/rango/models.py
+++ my_rango/rango/models.py
@@ -38,19 +38,16 @@ class Page(models.Model):
 
 
 class UserProfile(models.Model):
-    user=models.OneToOneField(User)
-    website=models.URLField(blank=True)
-    picture=models.ImageField(upload_to='profile_images',blank=True)
+    user = models.OneToOneField(User)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
 
     def __str__(self):
         return self.user.username
 
 
-#class Process_tag(models.Model):
-
-
 class PersonalInformation(models.Model):
-    name=models.CharField(verbose_name='姓名', max_length=10)
+    name = models.CharField(verbose_name='姓名', max_length=10)
     tel = models.CharField(verbose_name='电话', max_length=12)
     email = models.EmailField()
     gender = models.CharField(verbose_name='性别', max_length=1, choices=GENDER_CHOICES)
@@ -60,7 +57,7 @@ class PersonalInformation(models.Model):
     Date_of_Birth = models.DateField(verbose_name='出生日期', null=True, blank=True)
     duty = models.CharField(verbose_name='职务', max_length=1, choices=duty_choices)
     identity = models.CharField(verbose_name='对外身份', max_length=1, choices=identity_choices)
-    race = models.CharField(verbose_name='民族', max_length =2, choices=race_choices)
+    race = models.CharField(verbose_name='民族', max_length=2, choices=race_choices)
     political_identity = models.CharField(verbose_name='政治面貌', max_length=2, choices=political_choices)
     securety = models.CharField(verbose_name='涉密等级', max_length=1, choices=securety_choices)
     status_health = models.CharField(verbose_name='健康状况', max_length=1, choices=health_choices)
@@ -150,7 +147,7 @@ class Delegation(models.Model):
     finacial = models.CharField(verbose_name='课题卡号, 多个课题卡以分号隔开', max_length=100)
     budget_abroad = models.IntegerField(verbose_name='境外预算')
     budget_internal = models.IntegerField(verbose_name='国内预算')
-    process = models.CharField(verbose_name='团组进程', choices = process_tag, max_length=2, default='01')
+    process = models.CharField(verbose_name='团组进程', choices=process_tag, max_length=2, default='01')
     unexpected = models.CharField(verbose_name='团组特殊情况', max_length=1, choices=unexpected_tag, default='1')
     log = models.TextField(verbose_name='进度记录', blank=True)
 
@@ -162,21 +159,87 @@ class InvitationInformation(models.Model):
     delegation = models.ForeignKey(Delegation)
     time_start = models.DateField(verbose_name='邀请信上开始时间')
     time_end = models.DateField(verbose_name='邀请信上结束时间')
-#    member = models.ManyToManyField(Delegation.Members, verbose_name='被邀请人')
+    member = models.ManyToManyField(PersonalInformation, verbose_name='被邀请人')
     invitor = models.CharField(verbose_name='邀请人', max_length=100)
-    tel = models.CharField(verbose_name='邀请方电话',max_length=30)
+    tel = models.CharField(verbose_name='邀请方电话', max_length=30)
     address = models.CharField(verbose_name='邀请方英文地址', max_length=400)
     Institue_CH = models.CharField(verbose_name='邀请单位中文名称', max_length=200)
     Insitute_EN = models.CharField(verbose_name='邀请单位英文名称', max_length=200)
     invitation_letter = models.FileField(verbose_name='', upload_to='upload/%Y/%m/')
 
 
-
 class Permission(models.Model):
     delegation = models.ForeignKey(Delegation)
     inner_permit = models.FileField(verbose_name='所内审批表扫描件', upload_to='upload/%Y/%m/')
     cas_permit = models.FileField(verbose_name='院批件', upload_to='upload/%Y/%m/')
-    cas_permit_number = models.CharField(verbose_name='院批件文号', max_length=20)
+    cas_permit_number = models.CharField(verbose_name='院批件文号', max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.cas_permit_number
+
+
+class BudgetInformation(models.Model):
+    Delegation = models.ForeignKey(Delegation)
+    Budget_meal = models.IntegerField(verbose_name='伙食费（外币）')
+    Budget_accom = models.IntegerField(verbose_name='住宿费（外币）')
+    Budget_work = models.IntegerField(verbose_name='公杂费（外币）')
+    Budget_transition = models.IntegerField(verbose_name='城际间交通费（外币）', null=True, blank=True)
+    Budget_conference = models.IntegerField(verbose_name='会议注册费（外币）', null=True, blank=True)
+
+
+class SummaryInformation(models.Model):
+    Delegation = models.ForeignKey(Delegation)
+    date_leave = models.DateField(verbose_name='实际离境日期')
+    date_back = models.DateField(verbose_name='实际抵境日期')
+    cities = models.ManyToManyField(CityInformation, verbose_name='实际访问城市')
+    summary = models.TextField(verbose_name='出访总结')
+    passport = models.FileField(verbose_name='护照、出入境章扫描件', upload_to='upload/%Y/%m/')
+    receipt = models.FileField(verbose_name='发票扫描件', upload_to='upload/%Y/%m/')
+    picture = models.FileField(verbose_name='工作照片', upload_to='upload/%Y/%m/')
+    fee_meal = models.IntegerField(verbose_name='实际伙食费（外币）')
+    fee_accom = models.FloatField(verbose_name='实际住宿费（外币）')
+    fee_work = models.IntegerField(verbose_name='实际工杂费（外币）')
+    fee_transition = models.FloatField(verbose_name='实际城际间交通费（外币）')
+    fee_conference = models.FloatField(verbose_name='实际会议注册费（外币）')
+
+
+class Reimbursement(models.Model):
+    fee_meal_yan = models.FloatField(verbose_name='伙食费(人民币)')
+    fee_accom_uam = models.FloatField(verbose_name='住宿费(人民币)')
+    fee_work_yan = models.FloatField(verbose_name='工杂费(人民币)')
+    fee_transition_yan = models.FloatField(verbose_name='城际间交通费(人民币)')
+    fee_conference_yan = models.FloatField(verbose_name='会议注册费(人民币)')
+    cash_check = models.FileField(verbose_name='外汇核销表', upload_to='upload/%Y/%m/')
+    fee_international = models.FloatField(verbose_name='国际旅费')
+    fee_insurance = models.FloatField(verbose_name='保险费')
+    fee_visa = models.FloatField(verbose_name='签证费')
+    fee_transition = models.FloatField(verbose_name='市内交通费')
+    total = models.FloatField(verbose_name='合计')
+    def __str__(self):
+        return self.total
+
+
+class CardPay(models.Model):
+    Reim = models.ForeignKey(Reimbursement)
+    Card_num = models.CharField(verbose_name='课题卡号', max_length=20)
+    total = models.FloatField(verbose_name='国际交流费总金额')
+    current = models.FloatField(verbose_name='剩余国际交流费金额')
+    amount = models.FloatField(verbose_name='报销金额')
+
+    def __str__(self):
+        return self.Card_num + ' : ' + self.amount
+
+class Plan(models.Model):
+    person = models.ForeignKey(PersonalInformation)
+    country = models.ManyToManyField(CountryInformation)
+    elegation_tag = models.CharField(
+        max_length=2,
+        verbose_name='团组类型',
+        choices=delegation_tag_choices,
+    )
+    date_out = models.DateField(verbose_name='拟出访日期')
+    days_out = models.IntegerField(verbose_name='拟出访天数')
+    finacial = models.IntegerField(verbose_name='所需经费额')
 
 
 class TimeTable(models.Model):
@@ -200,4 +263,3 @@ class TimeTable(models.Model):
     passport_back_date = models.DateField(verbose_name='归还护照时间')
     cash_check_date = models.DateField(verbose_name='外汇核销时间')
     rmb_reimburse_date = models.DateField(verbose_name='完成报销时间')
-
