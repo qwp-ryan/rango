4e3c4c2e093ca9eea66b04658b0af43d39404d13
bifrostx <bifrost_xin@163.com>
2017-10-16 13:13:48 +0800

    - add registration functionality

--- my_rango/my_rango/urls.py
+++ my_rango/my_rango/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     url(r'^$',views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.default.urls')),
 ]
